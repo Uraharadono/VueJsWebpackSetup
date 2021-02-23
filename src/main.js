b95c8bbfda6e7ad11a423ba1e38cd9174866b69b
Uraharadono <35668552+Uraharadono@users.noreply.github.com>
2021-02-23 18:03:05 +0100

    Finished with menu icons

--- src/main.js
+++ src/main.js
@@ -5,11 +5,14 @@ import router from "./router";
 // Font awesome logic 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faHome, faAngleDoubleRight, faDatabase, faLayerGroup, faBook, faPencilAlt, faPalette,
-   faChalkboardTeacher, faTable, faMapMarkedAlt, faChartBar } 
-      from '@fortawesome/free-solid-svg-icons';
+   faChalkboardTeacher, faTable, faMapMarkedAlt, faChartBar, faSearch, faUser, faIdCard, faInbox, 
+   faCalendarAlt, faSlidersH, faSignOutAlt, faBell, faThumbsUp, faUserPlus, faShareAlt, faEnvelope } 
+   from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 library.add(faHome, faAngleDoubleRight, faDatabase, faLayerGroup, faBook, faPencilAlt, 
-            faPalette, faChalkboardTeacher, faTable, faMapMarkedAlt, faChartBar );
+            faPalette, faChalkboardTeacher, faTable, faMapMarkedAlt, faChartBar, faSearch, 
+            faUser, faIdCard, faInbox, faCalendarAlt, faSlidersH, faSignOutAlt, faBell,
+            faThumbsUp, faUserPlus, faShareAlt, faEnvelope );
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 
--- src/views/Menu/HorizontalNavbar.vue
+++ src/views/Menu/HorizontalNavbar.vue
@@ -1,5 +1,5 @@
 
-    <template>
+<template>
   <div class="nav-bar dark">
     <div class="nav-bar-wrap">
       <div class="nav-content">
@@ -14,7 +14,9 @@
           <div class="col title-col">
             <form id="page-search" class="search">
               <div class="input-container">
-                <span class="search-icon icofont icofont-search"></span>
+                <span class="search-icon">
+                  <font-awesome-icon icon="search" />
+                </span>
                 <input
                   placeholder="Type page title to search"
                   class="search-input"
@@ -30,13 +32,94 @@
                   class="nav-link d-none d-md-block"
                   id="toggle-search"
                   href="#"
-                  ><i class="icon icofont icofont-search"></i
-                ></a>
+                >
+                  <font-awesome-icon icon="search" />
+                </a>
               </div>
+
               <div class="item">
-                <a class="nav-link" data-toggle="dropdown" href="#"
-                  ><i class="icon icofont icofont-user"></i
-                ></a>
+                <a
+                  class="nav-link"
+                  data-toggle="dropdown"
+                  href="#"
+                  aria-expanded="false"
+                >
+                  <font-awesome-icon icon="bell" />
+                  <span class="marker blue"></span>
+                </a>
+
+                <div
+                  class="dropdown-menu dropdown-menu-right"
+                  x-placement="bottom-end"
+                  style="
+                    position: absolute;
+                    transform: translate3d(-190px, 44px, 0px);
+                    top: 0px;
+                    left: 0px;
+                    will-change: transform;
+                  "
+                >
+                  <div class="menu-header">
+                    <h4 class="menu-title">Notifications</h4>
+                  </div>
+                  <ul class="list menu-list">
+                    <li>
+                      <div class="icon">
+                        <font-awesome-icon icon="thumbs-up" />
+                      </div>
+                      <a href="#">
+                        <div class="content">
+                          <span class="desc"
+                            >Sara Crouch liked your photo
+                          </span>
+                          <span class="date">10 minutes ago</span>
+                        </div>
+                      </a>
+                    </li>
+                    <li>
+                      <div class="icon">
+                        <font-awesome-icon icon="user-plus" />
+                      </div>
+                      <a href="#">
+                        <div class="content">
+                          <span class="desc">New user registered</span>
+                          <span class="date">23 minutes ago</span>
+                        </div>
+                      </a>
+                    </li>
+                    <li>
+                      <div class="icon">
+                        <font-awesome-icon icon="share-alt" />
+                      </div>
+                      <a href="#">
+                        <div class="content">
+                          <span class="desc">Amanda Lie shared your post</span>
+                          <span class="date">25 minutes ago</span>
+                        </div>
+                      </a>
+                    </li>
+                    <li>
+                      <div class="icon">
+                        <font-awesome-icon icon="envelope" />
+                      </div>
+                      <a href="#">
+                        <div class="content">
+                          <span class="desc">You have new message</span>
+                          <span class="date">58 minutes ago</span>
+                        </div>
+                      </a>
+                    </li>
+                  </ul>
+                  <div class="menu-footer">
+                    <a href="#">View all notifications</a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="item">
+                <a class="nav-link" data-toggle="dropdown" href="#">
+                  <font-awesome-icon icon="user" />
+                </a>
                 <div
                   class="dropdown-menu dropdown-menu-right mini-menu profile"
                 >
@@ -49,29 +132,34 @@
                   </div>
                   <ul class="menu-list">
                     <li>
-                      <a href="#" class="profile-link"
-                        ><i class="icofont icofont-id-card"></i> Profile</a
-                      >
+                      <a href="#" class="profile-link">
+                        <font-awesome-icon icon="id-card" />
+                        Profile
+                      </a>
                     </li>
                     <li>
-                      <a href="#" class="profile-link"
-                        ><i class="icofont icofont-calendar"></i> Inbox</a
-                      >
+                      <a href="#" class="profile-link">
+                        <font-awesome-icon icon="inbox" />
+                        Inbox
+                      </a>
                     </li>
                     <li>
-                      <a href="#" class="profile-link"
-                        ><i class="icofont icofont-calendar"></i> Calendar</a
-                      >
+                      <a href="#" class="profile-link">
+                        <font-awesome-icon icon="calendar-alt" />
+                        Calendar
+                      </a>
                     </li>
                     <li>
-                      <a href="#" class="profile-link"
-                        ><i class="icofont icofont-settings"></i> Settings</a
-                      >
+                      <a href="#" class="profile-link">
+                        <font-awesome-icon icon="sliders-h" />
+                        Settings
+                      </a>
                     </li>
                     <li>
-                      <a href="#" class="profile-link"
-                        ><i class="icofont icofont-sign-out"></i> Log Out</a
-                      >
+                      <a href="#" class="profile-link">
+                        <font-awesome-icon icon="sign-out-alt" />
+                        Log Out
+                      </a>
                     </li>
                   </ul>
                 </div>
